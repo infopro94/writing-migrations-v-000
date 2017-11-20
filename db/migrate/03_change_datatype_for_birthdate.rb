245e80e57ca1a77153dec529ad4f4bd99039815f
Susan Fowler <infopro94@gmail.com>
2017-11-20 22:15:07 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,7 @@
 # class ChangeDatatypeForBirthdate < ActiveRecord::Migration
 #
 #   def change
-#     change_column :students do |s|
+#     change_column :students, :birthdate, :datetime
 #
 #
 # end
